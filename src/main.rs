a8b134081da062181bcb1f7bea0c9f3df0829faa
Jozef Hoschek <jozikh@hotmail.com>
2024-12-08 01:04:02 +0100

    prettier solution printing

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,9 @@ mod day03;
 fn main() {
     println!("Hello, world!");
     day01::day1("inputs/input-1.txt");
+    println!("--------------------------------------");
     day02::day2("inputs/input-2.txt");
+    println!("--------------------------------------");
     day03::day3("inputs/input-3.txt");
+    println!("--------------------------------------");
 }
