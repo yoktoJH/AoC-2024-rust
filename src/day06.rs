ced8120636f6fc23ed7b8840cbd023fbcf90e9a6
Jozef Hoschek <jozikh@hotmail.com>
2024-12-21 17:43:17 +0100

    day6 solved, part two is slow but works

--- src/day06.rs
+++ src/day06.rs
@@ -1,5 +1,6 @@
 use std::collections::{HashMap, HashSet};
 use std::fs;
+
 #[derive(Debug, Copy, Clone, Eq, Hash, PartialEq)]
 enum Direction {
     Up,
@@ -30,38 +31,26 @@ impl Direction {
 }
 
 fn is_viable_obstruction(
-    mut direction: Direction,
-    lab_history: &HashMap<(i32, i32), HashSet<Direction>>,
-    mut row: i32,
-    mut col: i32,
-    forbidden: &(i32, i32),
-    lab: &mut Vec<Vec<u8>>,
+    obs_row: i32,
+    obs_col: i32,
+    forbidden: (i32, i32),
+    lab: &mut  Vec<Vec<u8>>,
 ) -> bool {
-    let mut local_history: HashMap<(i32, i32), HashSet<Direction>> = HashMap::new();
-
-    let obstruction_pos = direction.move_guard(row, col);
-    if obstruction_pos == *forbidden {
+    if (obs_row,obs_col) == forbidden {
         return false;
     }
-    let old = lab[obstruction_pos.0 as usize][obstruction_pos.1 as usize];
-    lab[obstruction_pos.0 as usize][obstruction_pos.1 as usize] = b'#';
+
+    let mut local_history: HashMap<(i32, i32), HashSet<Direction>> = HashMap::new();
+
+    let mut direction = Direction::Up;
+
+    let old_tile = lab[obs_row as usize][obs_col as usize];
+    lab[obs_row as usize][obs_col as usize] = b'#';
+    let (mut row, mut col) = forbidden;
     loop {
         if !local_history.contains_key(&(row, col)) {
             local_history.insert((row, col), HashSet::new());
         }
-
-        if local_history[&(row, col)].contains(&direction)
-            || (lab_history.contains_key(&(row, col))
-                && lab_history[&(row, col)].contains(&direction))
-        {
-            lab[obstruction_pos.0 as usize][obstruction_pos.1 as usize] = old;
-            break;
-        }
-
-        local_history
-            .get_mut(&(row, col))
-            .unwrap()
-            .insert(direction);
 
         let (mut next_row, mut next_col) = direction.move_guard(row, col);
 
@@ -70,41 +59,56 @@ fn is_viable_obstruction(
             || next_row >= lab.len() as i32
             || next_col >= lab[next_row as usize].len() as i32
         {
-            lab[obstruction_pos.0 as usize][obstruction_pos.1 as usize] = old;
+            lab[obs_row as usize][obs_col as usize] = old_tile;
             return false;
         }
 
-        let tile_in_front = lab[next_row as usize][next_col as usize];
 
-        if b'#' == tile_in_front {
-            direction = direction.rotate();
-            (next_row, next_col) = direction.move_guard(row, col)
-        }
-
-        if next_row < 0
-            || next_col < 0
-            || next_row >= lab.len() as i32
-            || next_col >= lab[next_row as usize].len() as i32
+        if local_history[&(row, col)].contains(&direction)
         {
-            lab[obstruction_pos.0 as usize][obstruction_pos.1 as usize] = old;
-            return false;
+            break;
+        }
+
+        local_history
+            .get_mut(&(row, col))
+            .unwrap()
+            .insert(direction);
+
+        let mut tile_in_front = lab[next_row as usize][next_col as usize];
+
+        while b'#' == tile_in_front {
+            direction = direction.rotate();
+            (next_row, next_col) = direction.move_guard(row, col);
+
+            if next_row < 0
+                || next_col < 0
+                || next_row >= lab.len() as i32
+                || next_col >= lab[next_row as usize].len() as i32
+            {
+                lab[obs_row as usize][obs_col as usize] = old_tile;
+                return false;
+            }
+            tile_in_front = lab[next_row as usize][next_col as usize];
         }
 
         row = next_row;
         col = next_col;
-    }
 
-    lab[obstruction_pos.0 as usize][obstruction_pos.1 as usize] = old;
+    }
+    lab[obs_row as usize][obs_col as usize] = old_tile;
     true
 }
 
 pub(crate) fn day6(file_path: &str) {
+
     let input = fs::read_to_string(file_path).expect("Unable to read input file 6");
     let lines = input.lines();
+
     let mut row = 0;
     let mut col = None;
+
     let mut lab: Vec<Vec<u8>> = Vec::new();
-    let mut lab_history: HashMap<(i32, i32), HashSet<Direction>> = HashMap::new();
+    let mut lab_history: HashSet<(i32,i32)> = HashSet::new();
 
     for (i, line) in lines.enumerate() {
         lab.push(line.as_bytes().to_vec());
@@ -117,19 +121,18 @@ pub(crate) fn day6(file_path: &str) {
 
     let mut row = row as i32;
     let mut col = col.expect("Nothing was found, that is really bad!") as i32;
-    let mut uniquely_visited_tiles = 0;
     let mut direction = Direction::Up;
     let forbidden_place = (row, col);
-    let mut obstructions:HashSet<(i32,i32)> = HashSet::new();
+
+
     loop {
-        if !lab_history.contains_key(&(row, col)) {
-            lab_history.insert((row, col), HashSet::new());
-        }
+
+        lab_history.insert((row,col));
 
         if lab[row as usize][col as usize] != b'X' {
-            uniquely_visited_tiles += 1;
             lab[row as usize][col as usize] = b'X';
         }
+
         let (mut next_row, mut next_col) = direction.move_guard(row, col);
         if next_row < 0
             || next_col < 0
@@ -143,15 +146,6 @@ pub(crate) fn day6(file_path: &str) {
         if b'#' == tile_in_front {
             direction = direction.rotate();
             (next_row, next_col) = direction.move_guard(row, col)
-        } else if is_viable_obstruction(
-            direction,
-            &lab_history,
-            row,
-            col,
-            &forbidden_place,
-            &mut lab,
-        ) {
-           obstructions.insert((row,col));
         }
 
         if next_row < 0
@@ -162,12 +156,20 @@ pub(crate) fn day6(file_path: &str) {
             break;
         }
 
-        lab_history.get_mut(&(row, col)).unwrap().insert(direction);
         row = next_row;
         col = next_col;
     }
+    lab_history.remove(&forbidden_place);
 
-    println!("Solution for day 6 part 1: {uniquely_visited_tiles}");
-    //1600 too high and 239 is too low
-    println!("Solution for day 6 part 2: {}",obstructions.len());
+    let mut valid_obstructions = 0;
+    for (ob_row,ob_col) in &lab_history{
+        if is_viable_obstruction(*ob_row,*ob_col,forbidden_place,&mut lab){
+            valid_obstructions+=1;
+        }
+    }
+    //plus 1 because I removed the starting position
+    println!("Solution for day 6 part 1: {}",lab_history.len() +1);
+
+    println!("Solution for day 6 part 2: {valid_obstructions}");
+
 }
